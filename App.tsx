3cc9cbb74d29e7e173e87d9ecdca86f8cb890900
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2021-12-25 10:55:01 -0600

    CRUD with firebase

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, Button } from "react-native"
 import { db } from "./firebase"
 
 export default function App() {
@@ -8,15 +8,57 @@ export default function App() {
     .then((querySnapshot) => {
       console.log("Total users: ", querySnapshot.size)
 
-      querySnapshot.forEach((documentSnapshot) => {
-        console.log("User ID: ", documentSnapshot.id, documentSnapshot.data())
-      })
+      console.log("here we are,  ", querySnapshot.docs[0].data())
+      // querySnapshot.forEach((documentSnapshot) => {
+      //   console.log("User ID: ", documentSnapshot.id, documentSnapshot.data())
+      // })
     })
+
+  const addUser = () => {
+    const database = db.collection("users")
+    database
+      .add({
+        name: "Stanley Wan",
+        age: 32,
+      })
+      .then((res) => {
+        console.log("res, ", JSON.stringify(res))
+      })
+      .catch((err) => {
+        console.log("err, ", err)
+      })
+  }
+
+  const udpateUser = () => {
+    const database = db.collection("users")
+    database
+      .doc("QW5sTSwuReMRCK0q3Ylz")
+      .set({ age: 19 })
+      .then((res) => {
+        console.log("update")
+      })
+      .catch((err) => {
+        console.log("err, ", err)
+      })
+  }
+
+  const deleteUser = () => {
+    const database = db
+      .collection("users")
+      .doc("QW5sTSwuReMRCK0q3Ylz")
+      .delete()
+      .then(() => {
+        console.log("work")
+      })
+  }
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on Stanley's app!</Text>
       <StatusBar style="auto" />
+      <Button title="SUBMIT" onPress={addUser}></Button>
+      <Button title="UPDATE" onPress={udpateUser}></Button>
+      <Button title="DELETE" onPress={deleteUser}></Button>
     </View>
   )
 }
--- firebase.ts
+++ firebase.ts
@@ -1,16 +1,6 @@
 import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 import "firebase/compat/firestore"
-import {
-  apiKey,
-  authDomain,
-  databaseURL,
-  projectId,
-  storageBucket,
-  messagingSenderId,
-  appId,
-  measurementId,
-} from "@env"
 
 const firebaseConfig = {
   apiKey: process.env.apiKey,
