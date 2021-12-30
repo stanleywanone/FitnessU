bd9eefab20ca65313b47ae5ea2dc589365cd0f1e
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2021-12-30 15:11:38 -0600

    add library react native sound

--- App.tsx
+++ App.tsx
@@ -7,55 +7,6 @@ import { Database } from "./core/components/Database/Database"
 import { Routine } from "./core/components/Routine/Routine"
 
 export default function App() {
-  // db.collection("users")
-  //   .get()
-  //   .then((querySnapshot) => {
-  //     console.log("Total users: ", querySnapshot.size)
-
-  //     console.log("here we are,  ", querySnapshot.docs[0].data())
-  //     // querySnapshot.forEach((documentSnapshot) => {
-  //     //   console.log("User ID: ", documentSnapshot.id, documentSnapshot.data())
-  //     // })
-  //   })
-
-  // const addUser = () => {
-  //   const database = db.collection("users")
-  //   database
-  //     .add({
-  //       name: "Stanley Wan",
-  //       age: 32,
-  //     })
-  //     .then((res) => {
-  //       console.log("res, ", JSON.stringify(res))
-  //     })
-  //     .catch((err) => {
-  //       console.log("err, ", err)
-  //     })
-  // }
-
-  // const udpateUser = () => {
-  //   const database = db.collection("users")
-  //   database
-  //     .doc("QW5sTSwuReMRCK0q3Ylz")
-  //     .set({ age: 19 })
-  //     .then((res) => {
-  //       console.log("update")
-  //     })
-  //     .catch((err) => {
-  //       console.log("err, ", err)
-  //     })
-  // }
-
-  // const deleteUser = () => {
-  //   const database = db
-  //     .collection("users")
-  //     .doc("QW5sTSwuReMRCK0q3Ylz")
-  //     .delete()
-  //     .then(() => {
-  //       console.log("work")
-  //     })
-  // }
-
   return (
     <NativeBaseProvider>
       <ScrollView style={styles.container}>
